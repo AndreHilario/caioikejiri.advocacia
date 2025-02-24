0499e8f67d1bc560031c3e489028d85fe68598f3
André Ikejiri Hilario <andre.hilario@teros.com.br>
2025-02-24 17:17:28 -0300

    fix: errors to build correctly

--- src/lib/muiThemes/muiTheme.ts
+++ src/lib/muiThemes/muiTheme.ts
@@ -1,58 +1,26 @@
-import { muiComponents } from './themes/muiComponentsTheme';
-import { muiTypography } from './themes/muiTypographyTheme';
-
-import React from 'react';
-import { createTheme } from '@mui/material';
+import { createTheme } from "@mui/material";
+import { muiTypography } from "./themes/muiTypographyTheme";
+import { muiComponents } from "./themes/muiComponentsTheme";
 
 declare module '@mui/material/styles' {
-  interface Typography {
-    blockQuotes: React.CSSProperties;
-    capitalized: React.CSSProperties;
-    small: React.CSSProperties;
-    tiny: React.CSSProperties;
-  }
-  interface TypographyOptions {
-    blockQuotes: React.CSSProperties;
-    capitalized: React.CSSProperties;
-    small: React.CSSProperties;
-    tiny: React.CSSProperties;
-  }
-
   interface TypographyVariants {
-    blockQuotes: React.CSSProperties;
+    blockquote: React.CSSProperties;
     capitalized: React.CSSProperties;
     small: React.CSSProperties;
     tiny: React.CSSProperties;
+    letterSpacingVariant: React.CSSProperties;
   }
 
   interface TypographyVariantsOptions {
-    blockQuotes: React.CSSProperties;
-    capitalized: React.CSSProperties;
-    small: React.CSSProperties;
-    tiny: React.CSSProperties;
-  }
-  interface TypographyPropsVariantOverrides {
-    blockQuotes: true;
-    capitalized: true;
-    small: true;
-    tiny: true;
-  }
-  interface PaletteOptions {
-    alternativeColors: {
-      black: string;
-      white: string;
-      mainLighter: string;
-      defaultBackgroundColor: string;
-      customBackgroundColor: string;
-      tableRowGray: string;
-      tableRowHover: string;
-      tableRowEvenSelected: string;
-    };
+    blockquote?: React.CSSProperties;
+    capitalized?: React.CSSProperties;
+    small?: React.CSSProperties;
+    tiny?: React.CSSProperties;
+    letterSpacingVariant?: React.CSSProperties;
   }
 }
 
 export const muiTheme = createTheme({
   typography: muiTypography,
-
   components: muiComponents.components,
 });
