9aec5f185b6637e0b96b59de284b8e18b581d43c
AndreHilario <andre18hilario@gmail.com>
2025-03-03 16:45:04 -0300

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(() => {
     plugins: [react(), tailwindcss()],
     host: true,
     strictPort: true,
-    port: 3000
+    port: 3000,
+    base: "https://github.com/AndreHilario/caioikejiri.advocacia"
   };
 });
