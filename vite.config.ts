fb4d9538137060c0d45b312e442ff852b0965177
AndreHilario <andre18hilario@gmail.com>
2025-03-04 14:27:51 -0300

    feat: add dist to build try

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/', // ou '/nome-do-repositorio/' se estiver hospedando em um subpath
-    server: {
-      headers: {
-        'Content-Type': 'application/javascript',
-      },
-    },
     strictPort: true,
     port: 3000,
-    build: {
-      outDir: 'docs', // Diretório de saída padrão, pode ser mantido
-      assetsDir: 'assets', // Opcional: diretório para assets (imagens, etc.)
-      rollupOptions: {
-        output: {
-          assetFileNames: 'assets/[name]-[hash][extname]', // Adiciona hash para cache busting
-          chunkFileNames: 'chunks/[name]-[hash][extname]', // Adiciona hash para cache busting
-          entryFileNames: 'entry/[name]-[hash].js',       // Adiciona hash para cache busting
-        },
-      },
-    },
   };
 });
